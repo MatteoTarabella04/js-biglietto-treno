151d4c00097f3e3d10647994e2f92801d8095ae9
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-08 16:09:36 +0100

    add variables

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+// chiedo all'utente km e età e li assegno ad una variabile
+
+const km = prompt('Quanti chilometri devi percorrere?');
+console.log(km);
+
+const age = prompt('Quanti anni hai?');
+console.log(age);
+
+// dichiaro la variabile del prezzo per chilometro
+
+const priceKm = 0.21;
+
+// dichiaro la variabile del totale senza assegnarle nessun valore
+
+let tot;
