9587f6b5d8e85b2ef990bccf637d7ac5d657a528
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-08 16:20:20 +0100

    add control for total price

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,25 @@ const priceKm = 0.21;
 // dichiaro la variabile del totale senza assegnarle nessun valore
 
 let tot;
+const finalPrice = document.getElementById('priceTot')
+
+// verifico le situazioni e applico gli sconti dove posso
+
+if (age < 18) {
+    tot = (km * priceKm) * 20 / 100;
+    console.log(tot.toFixed(2));
+
+    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
+
+} else if (age > 65) {
+    tot = (km * priceKm) * 40 / 100;
+    console.log(tot.toFixed(2));
+
+    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
+
+} else {
+    tot = (km * priceKm);
+
+    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
+
+}
