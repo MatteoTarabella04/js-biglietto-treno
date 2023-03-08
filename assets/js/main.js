8fadf9e939c1b3eda93abc550fda3f3499a039c5
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-08 17:04:33 +0100

    add more controls + fix innerHTML out of if

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-
 // chiedo all'utente km e età e li assegno ad una variabile
-
 const km = prompt('Quanti chilometri devi percorrere?');
 console.log(km);
 
@@ -8,32 +6,32 @@ const age = prompt('Quanti anni hai?');
 console.log(age);
 
 // dichiaro la variabile del prezzo per chilometro
-
 const priceKm = 0.21;
 
 // dichiaro la variabile del totale senza assegnarle nessun valore
-
 let tot;
 const finalPrice = document.getElementById('priceTot')
 
-// verifico le situazioni e applico gli sconti dove posso
+// aggiungo un ulteriore controllo affinche l'utente inserisca i valori richiesti nel modo corretto
+if (isNaN(km) || isNaN(age)) {
+    alert('ERRORE! Il valore da inserire deve essere di tipo numerico');
 
+}
+
+
+// verifico le situazioni e applico gli sconti dove posso
 if (age < 18) {
     tot = (km * priceKm) * 20 / 100;
     console.log(tot.toFixed(2));
-
-    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
 
 } else if (age > 65) {
     tot = (km * priceKm) * 40 / 100;
     console.log(tot.toFixed(2));
 
-    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
-
 } else {
     tot = (km * priceKm);
 
-    finalPrice.innerHTML = `${tot.toFixed(2)} €`;
     console.log(tot.toFixed(2));
-
 }
+
+finalPrice.innerHTML = `${tot.toFixed(2)} €`;
